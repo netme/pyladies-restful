f62fe5214038bcb1515df9d8767f0f0a8d4d6042
Mikhail Chernykh <netmisch@gmail.com>
2015-04-26 22:22:15 +0200

    Add Book model

--- bookstore/api/models.py
+++ bookstore/api/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return '{name} ({price})'.format(name=self.name, price=self.price)
